f55ad1f5b19c2e9e8896b0d2825f89675aab0f0b
ArthurVillela <arthurvillela@hotmail.com>
2021-09-16 15:36:25 -0300

    Exercise 03 done, 02 on going

--- src/br/com/letscode/java/Main.java
+++ src/br/com/letscode/java/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
 	    //questao01();
-	    questao02();
-	    //questao03();
+	    //questao02();
+        //questao03();
 	    //questao04();
 	    //questao05();
 	    //questao06();
@@ -26,10 +26,59 @@ public class Main {
         String[] carrinhoCompras = new String[5];
 
         for(int i = 0; i < carrinhoCompras.length; i++){
-            System.out.printf("Informe o nome da %dº fruta\n",i+1);
+            System.out.printf("Informe o nome da %dº fruta: \n",i+1);
             carrinhoCompras[i] = in.nextLine();
         }
         System.out.println(Arrays.toString(carrinhoCompras));
     }
 
-}
+    public static void questao02() {
+        /*
+            Escreva um programa que lê uma palavra e a escreve de volta ao contrário.
+        */
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite uma palavra: ");
+        String palavra = in.nextLine();
+
+
+
+
+        }
+
+    public static void questao03() {
+
+        /*
+            Escreva um programa que lê 5 números. Ao final, escreva primeiros todos os ímpares, depois todos os pares.
+        */
+
+        Scanner in = new Scanner(System.in);
+
+        int[] nums = new int[5];
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.printf("Digite o %dº valor: \n",(i+1));
+            nums[i] = in.nextInt();
+
+        }
+        //  System.out.println(Arrays.toString(nums));
+        System.out.println("Numeros impares: ");
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i]%2!=0){
+                System.out.print(nums[i] + ";");
+            }
+        }
+
+        System.out.println("\nNumeros pares: ");
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i]%2==0){
+                System.out.print(nums[i] + ";");
+            }
+        }
+
+
+
+    }
+    }
+
+
